d8e8f7e14d7f80f92bbeadc0f26d537a8eba6fdd
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-17 16:37:13 -0500

    Trabajando en la Peticion POST

--- server/server.js
+++ server/server.js
@@ -3,23 +3,41 @@ require('./config/config');
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+//Peticion GET
 app.get('/usuario', function (req, res) {
   res.json('Peticion  GET ejecutada!');
 });
 
+//Peticion POST
 app.post('/usuario', function (req, res) {
-    res.json('Peticion POST ejecutada!');
+    let body = req.body;
+
+        res.json({
+            persona: body
+        });
+    
   });
 
-  app.put('/usuario', function (req, res) {
-    res.json('Peticion PUT ejecutada!');
+  app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+
+    res.json({
+        id: id
+    });
   });
 
   app.delete('/usuario', function (req, res) {
     res.json('Peticion DELETE ejecutada!');
   });
   
-
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
