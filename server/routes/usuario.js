072c99cd18497b0aefa311fb72bf31377e8eaafb
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-19 01:33:13 -0500

    filtrando los campos de la lista de usuarios

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -14,7 +14,7 @@ app.get("/usuario", function (req, res) {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-    Usuario.find({})
+    Usuario.find({ estado: true }, 'nombre email role estado google img')
         .skip(desde)
         .limit(limite)
         .exec((err, usuarios) => {
