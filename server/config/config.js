67f4f67e035b33517fe095189c3d8d5e860b8327
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-19 12:39:09 -0500

    Update Package.json

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 
-// ============================ BD
+// ========= BD =========
 let urlDB;
 
 if (process.env.NODE_ENV || "dev") {
