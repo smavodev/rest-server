c53e6441e28f69ff80882dbd2c888f51dc4ddafe
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-18 23:11:22 -0500

    add Mongoose database conection

--- server/server.js
+++ server/server.js
@@ -1,56 +1,68 @@
-require('./config/config');
+require("./config/config");
 
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //Peticion GET
-app.get('/usuario', function (req, res) {
-  res.json('Peticion  GET ejecutada');
+app.get("/usuario", function (req, res) {
+  res.json("Peticion  GET ejecutada");
 });
 
 //Peticion POST
-app.post('/usuario', function (req, res) {
+app.post("/usuario", function (req, res) {
+  let body = req.body;
 
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-  });
-
-  app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-
-    res.json({
-        id: id
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "El nombre es necesario",
     });
-  });
-
-  app.delete('/usuario', function (req, res) {
-    res.json('Peticion DELETE ejecutada!');
-  });
-  
-
-app.listen(process.env.PORT, () => {
-    console.log('Escuchando puerto: ', process.env.PORT);
-    console.log('Puerta de enlace '+'http://localhost:'+ process.env.PORT);
+  } else {
+    res.json({
+      persona: body,
+    });
+  }
 });
-  
 
+app.put("/usuario/:id", function (req, res) {
+  let id = req.params.id;
+
+  res.json({
+    id: id,
+  });
+});
+
+app.delete("/usuario", function (req, res) {
+  res.json("Peticion DELETE ejecutada!");
+});
+
+// Conexion a base de datos
+mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
+  if (err) throw err;
+  else{
+    console.log('Base de datos Conectada');
+
+    app.listen(process.env.PORT, () => {
+      /*console.log("Escuchando puerto: ", process.env.PORT);*/
+      console.log("Servidor Conectado " + "http://localhost:" + process.env.PORT);
+    });
+
+  }
+});
+
+/*
+app.listen(process.env.PORT, () => {
+  console.log("Escuchando puerto: ", process.env.PORT);
+  console.log("Puerta de enlace " + "http://localhost:" + process.env.PORT);
+});
+*/
