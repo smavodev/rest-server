84e5fcabc670f804d2dab128fd7d4794a3034c48
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-18 23:56:16 -0500

    Agregando Usuario a la BD_Cafe [POST]

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const Usuario = require('../models/usuario');
 const app = express();
 
 //Peticion GET
@@ -11,16 +12,26 @@ app.get("/usuario", function (req, res) {
   app.post("/usuario", function (req, res) {
     let body = req.body;
   
-    if (body.nombre === undefined) {
-      res.status(400).json({
-        ok: false,
-        mensaje: "El nombre es necesario",
-      });
-    } else {
-      res.json({
-        persona: body,
-      });
-    }
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: body.password,
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
   });
   
   app.put("/usuario/:id", function (req, res) {
