98aed402dcd5c25d8ceed3d43d62837122a7cf75
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-19 13:24:17 -0500

    Despliegue de conexion a base de dato con validacion

--- server/config/config.js
+++ server/config/config.js
@@ -8,17 +8,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || "dev";
 
 // ========= BD =========
 let urlDB;
-/*
-if (process.env.NODE_ENV || "dev") {
+
+if (process.env.NODE_ENV === "dev") {
   // BD_LOCAL
   urlDB = "mongodb://localhost:27017/cafe";
 
 } else {
   //BD_REMOTA
   urlDB = "mongodb+srv://smavo:WFIuYEL9YVdHr1zS@cluster0-qf1lr.mongodb.net/cafe";
-}*/
-
-urlDB = "mongodb+srv://smavo:WFIuYEL9YVdHr1zS@cluster0-qf1lr.mongodb.net/cafe";
-
+}
 
 process.env.URLDB = urlDB;
