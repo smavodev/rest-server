1df4a8d55a78fbb3fa5db7c6652a0f12b3b6c593
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-06-21 00:35:48 -0500

    Crear usuario con credenciales de google, autentificacion de usuario por google

--- server/routes/login.js
+++ server/routes/login.js
@@ -69,9 +69,17 @@ async function verify( token) {
     });
     const payload = ticket.getPayload();
 
-    console.log(payload.name);
+    /*console.log(payload.name);
     console.log(payload.email);
-    console.log(payload.picture);
+    console.log(payload.picture);*/
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+
   }
 
 
@@ -79,12 +87,84 @@ app.post('/google', async(req, res) => {
 
      let token = req.body.idtoken;
 
-    verify(token); 
+     let googleUser = await verify(token)
+     .catch(e => {
+         return res.status(403).json({
+             ok: false,
+             err: e
+         });
+     });
 
-    return res.json({
-        token
-    });
 
+     Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        };
+        
+
+        if (usuarioDB) {
+
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe de usar su autenticación normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token,
+                });
+
+            }
+
+            
+        } else {
+            // Si el usuario no existe en nuestra base de datos
+            let usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = 'XXXX-XXXX';
+
+            usuario.save((err, usuarioDB) => {
+
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                };
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token,
+                });
+            });
+        }
+
+     });    
+
+    
 });
 
 
